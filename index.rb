6deaad7801cce3b9300c8119791020dffae11639
takumi-shibata <241448ta@gmail.com>
2020-09-05 23:53:36 +0900

    １周目 学習終了

--- index.rb
+++ index.rb
@@ -105,3 +105,57 @@ puts "個数を入力してください(3つ以上で100円割引)"
 count = gets.chomp.to_i
 
 puts "お会計は#{selected_menu.get_total_price(count)}円です"
+
+
+
+# Dateクラス(DateクラスとはRubyがすでに用意しているクラスで、日付を扱う時に用いる)
+# ↓「require」を用いてDateクラスを読み込むことで、クラスを定義しなくても使うことができる(※すでに用意されているクラスは、requireの書き方が少し異なるので注意！)
+require "date" # ←(※"./dateではないことに注意")
+
+# ↓今まで扱ってきたクラスと同様「クラス名.new」とすることでインスタンスを生成することができる(※引数に年,月,日を渡してインスタンスを生成)
+birthday = Date.new(1996, 3, 1)
+puts birthday
+# →1996-03-01
+
+# ↓Dateクラスのインスタンスメソッド:「sunday?」
+#「sunday?」とはDateクラスのインスタンスの日付が、「日曜日かどうか」を真偽値で返すメソッド(合っていたらtrue、間違っていたらfalse)
+puts birthday.sunday?
+# →false
+
+# ↓「Date.today」今日の日付のインスタンスをつくることができる
+today = Date.today
+puts today
+# →2020-09-05
+
+
+
+# クラスメソッドの呼び出し
+require "./food"
+require "./drink"
+
+puts "日曜限定100円割引セール実施中！"
+
+food1 = Food.new(name: "ピザ", price: 800, calorie: 700)
+food2 = Food.new(name: "すし", price: 1000, calorie: 600)
+drink1 = Drink.new(name: "コーラ", price: 300, amount: 500)
+drink2 = Drink.new(name: "お茶", price: 200, amount: 400)
+
+menus = [food1, food2, drink1, drink2]
+
+index = 0
+menus.each do |menu|
+  puts "#{index}. #{menu.info}"
+  index += 1
+end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+order = gets.chomp.to_i
+
+selected_menu = menus[order]
+puts "選択されたメニュー: #{selected_menu.name}"
+
+puts "個数を入力してください(3つ以上で100円割引)"
+count = gets.chomp.to_i
+
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
--- menu.rb
+++ menu.rb
@@ -19,3 +19,82 @@ class Menu
     return total_price
   end
 end
+
+
+
+# クラスメソッド(クラスに対して呼び出すメソッド)
+#「def クラス名.メソッド名 end」とすることで定義できる
+
+# ↓Dateクラスを読み込み
+require "date"
+
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+
+  def initialize(name:, price:)
+    self.name = name
+    self.price = price
+  end
+  
+  def info
+    return "#{self.name} #{self.price}円"
+  end
+  
+  def get_total_price(count)
+    total_price = self.price * count
+    if count >= 3
+      total_price -= 100
+    end
+    return total_price
+  end
+  
+  # ↓クラスメソッド
+  def Menu.is_discount_day?
+
+    # ↓「Date.today」今日の日付のインスタンスをつくることができる
+    today = Date.today
+
+    # ↓Dateクラスのインスタンスメソッド:「sunday?」
+    # ↓戻り値で日曜日であればtrue、そうでなければfalseを返す処理   
+    return today.sunday?
+  end
+end
+
+
+
+# クラスメソッドの呼び出し(※インスタンスメソッド内で呼び出す事ができる)
+# ※インスタンスメソッドはインスタンスに対して呼び出す→(例)インスタンスを生成した変数名.メソッド名。 クラスメソッドはクラスに対して呼び出す→(例)クラス名.メソッド名
+require "date"
+
+class Menu
+  attr_accessor :name
+  attr_accessor :price
+
+  def initialize(name:, price:)
+    self.name = name
+    self.price = price
+  end
+  
+  def info
+    return "#{self.name} #{self.price}円"
+  end
+  
+  def get_total_price(count)
+    total_price = self.price * count
+    if count >= 3
+      total_price -= 100
+    end
+    
+    # ↓インスタンスメソッド内でクラスメソッドの呼び出し
+    if count >= 1 && Menu.is_discount_day? == true # ←数量が「1以上」かつ、クラスメソッドの処理で「今日の日付が日曜日(true)」ならば合計金額から100円引き
+      total_price -= 100
+    end
+    return total_price
+  end
+  
+  def Menu.is_discount_day?
+    today = Date.today
+    return today.sunday?
+  end
+end
